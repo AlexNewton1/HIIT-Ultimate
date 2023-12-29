316a7a3b0204c9007d05140bd85983aee8d980ac
Software Overflow <alex_newton@btinternet.com>
2023-12-29 18:12:57 +0000

    Remove logs

--- app/src/main/java/com/softwareoverflow/hiit_trainer/data/history/WorkoutHistoryRoomDb.kt
+++ app/src/main/java/com/softwareoverflow/hiit_trainer/data/history/WorkoutHistoryRoomDb.kt
@@ -2,7 +2,6 @@ package com.softwareoverflow.hiit_trainer.data.history
 
 import com.softwareoverflow.hiit_trainer.repository.dto.history.WorkoutHistoryDTO
 import com.softwareoverflow.hiit_trainer.ui.workout.WorkoutSection
-import timber.log.Timber
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -12,7 +11,6 @@ class WorkoutHistoryRoomDb @Inject constructor(private val historyDao: WorkoutHi
     override suspend fun createOrUpdate(workoutHistory: WorkoutHistoryDTO) {
         if (workoutHistory.type == WorkoutSection.PREPARE) return
 
-        Timber.d("Before name change: ${workoutHistory.name}")
         // We need to update the name before we search for the matching entry in the database
         workoutHistory.name =
             when (workoutHistory.type) {
@@ -20,7 +18,6 @@ class WorkoutHistoryRoomDb @Inject constructor(private val historyDao: WorkoutHi
                 WorkoutSection.REST -> WorkoutSection.REST.name
                 else -> workoutHistory.name
             }
-        Timber.d("Post name change: ${workoutHistory.name}")
 
         val dateString = HistoryConverters.historyDateFormat.format(workoutHistory.date)
 
@@ -30,13 +27,9 @@ class WorkoutHistoryRoomDb @Inject constructor(private val historyDao: WorkoutHi
         if (existingData != null) {
             // If we have a match we need to update the time
             workoutHistory.time += existingData.time
-
-            Timber.d("Found existing entry: ${existingData.time}s for section ${existingData.type} named ${existingData.name}\nValue updated to ${workoutHistory.time}")
         }
 
-        Timber.d("Writing to database: ${workoutHistory.time}s for section ${workoutHistory.type} named ${workoutHistory.name}")
         historyDao.createOrUpdate(workoutHistory.toEntity())
-        Timber.d("\n\n")
     }
 
     override suspend fun getAllHistory(): List<WorkoutHistoryDTO> {
--- app/src/main/java/com/softwareoverflow/hiit_trainer/ui/history/write/HistorySaverLocal.kt
+++ app/src/main/java/com/softwareoverflow/hiit_trainer/ui/history/write/HistorySaverLocal.kt
@@ -2,7 +2,6 @@ package com.softwareoverflow.hiit_trainer.ui.history.write
 
 import com.softwareoverflow.hiit_trainer.repository.dto.history.WorkoutHistoryDTO
 import com.softwareoverflow.hiit_trainer.ui.workout.WorkoutSection
-import timber.log.Timber
 import java.time.LocalDate
 import javax.inject.Inject
 
@@ -11,7 +10,6 @@ class HistorySaverLocal @Inject constructor (private val historyWriter: IHistory
     var history : WorkoutHistoryDTO? = null
 
     override fun addHistory(seconds: Int, section: WorkoutSection, name: String) {
-        Timber.d("Trying to add history: ${seconds}s to $section for $name")
         val dateNow = LocalDate.now()
 
         if(history == null) {
@@ -31,19 +29,15 @@ class HistorySaverLocal @Inject constructor (private val historyWriter: IHistory
                 WorkoutSection.RECOVER -> this.time += seconds
                 else -> { /* Do Nothing */ }
             }
-
-            Timber.d("Cached history: ${this.time}s to ${this.type} for ${this.name}")
         }
     }
 
     private fun createDTO(section: WorkoutSection, name: String, date: LocalDate){
-        Timber.d("Mismatch found. Creating new DTO.")
         history = WorkoutHistoryDTO(1, name, section, date)
     }
 
     override fun write() {
         history?.let {
-            Timber.d("Attempting to write history: ${it.time}s, section ${it.type}, named ${it.name}")
             historyWriter.writeHistory(it.copy())
 
             history = null
