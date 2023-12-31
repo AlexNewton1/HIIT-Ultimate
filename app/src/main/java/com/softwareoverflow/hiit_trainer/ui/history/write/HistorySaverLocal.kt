9682e4f2d2fe98d4eb40c5fe02bde639ba8346f7
Software Overflow <alex_newton@btinternet.com>
2023-12-31 13:41:31 +0000

    Adding error checking

--- app/src/main/java/com/softwareoverflow/hiit_trainer/ui/history/write/HistorySaverLocal.kt
+++ app/src/main/java/com/softwareoverflow/hiit_trainer/ui/history/write/HistorySaverLocal.kt
@@ -1,46 +1,59 @@
 package com.softwareoverflow.hiit_trainer.ui.history.write
 
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import com.softwareoverflow.hiit_trainer.repository.dto.history.WorkoutHistoryDTO
 import com.softwareoverflow.hiit_trainer.ui.workout.WorkoutSection
 import java.time.LocalDate
 import javax.inject.Inject
 
-class HistorySaverLocal @Inject constructor (private val historyWriter: IHistoryWriter) : IHistorySaver {
+class HistorySaverLocal @Inject constructor(private val historyWriter: IHistoryWriter) :
+    IHistorySaver {
 
-    var history : WorkoutHistoryDTO? = null
+    var history: WorkoutHistoryDTO? = null
 
     override fun addHistory(seconds: Int, section: WorkoutSection, name: String) {
-        val dateNow = LocalDate.now()
+        try {
+            val dateNow = LocalDate.now()
 
-        if(history == null) {
-            createDTO(section, name, dateNow)
-        }
-
-        history?.apply {
-            if( this.date != dateNow || section != this.type || name != this.name) {
-                write()
-
+            if (history == null) {
                 createDTO(section, name, dateNow)
             }
 
-            when (section) {
-                WorkoutSection.WORK -> this.time += seconds
-                WorkoutSection.REST -> this.time += seconds
-                WorkoutSection.RECOVER -> this.time += seconds
-                else -> { /* Do Nothing */ }
+            history?.apply {
+                if (this.date != dateNow || section != this.type || name != this.name) {
+                    write()
+
+                    createDTO(section, name, dateNow)
+                }
+
+                when (section) {
+                    WorkoutSection.WORK -> this.time += seconds
+                    WorkoutSection.REST -> this.time += seconds
+                    WorkoutSection.RECOVER -> this.time += seconds
+                    else -> { /* Do Nothing */
+                    }
+                }
             }
+        } catch (e: Exception) {
+            FirebaseCrashlytics.getInstance().recordException(e)
         }
+
+
     }
 
-    private fun createDTO(section: WorkoutSection, name: String, date: LocalDate){
+    private fun createDTO(section: WorkoutSection, name: String, date: LocalDate) {
         history = WorkoutHistoryDTO(1, name, section, date)
     }
 
     override fun write() {
-        history?.let {
-            historyWriter.writeHistory(it.copy())
+        try {
+            history?.let {
+                historyWriter.writeHistory(it.copy())
 
-            history = null
+                history = null
+            }
+        } catch(e: Exception) {
+            FirebaseCrashlytics.getInstance().recordException(e)
         }
     }
 }
